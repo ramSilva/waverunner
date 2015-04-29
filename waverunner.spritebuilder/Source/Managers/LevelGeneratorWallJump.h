b4c26ed2169c4b3558f1b9621aa885ccd668ab3c
justiceiro <crispedros@oninet.pt>
2015-04-29 15:08:26 +0100

    Updated Values in WallJump & added obstacles wj

--- waverunner.spritebuilder/Source/Managers/LevelGeneratorWallJump.h
+++ waverunner.spritebuilder/Source/Managers/LevelGeneratorWallJump.h
@@ -16,15 +16,18 @@ static int const MIN_NUMBER_WALLS = 2;
 //2 * MAX_MULT_WALLS = MAX_NUMBER_WALLS
 static int const MAX_MULT_WALLS = 5;
 static int const MIN_DISTANCE_WALLS = 200.0f;
-static int const MAX_DISTANCE_WALLS = 400.0f;
+static int const MAX_DISTANCE_WALLS = 350.0f;
 
 @interface LevelGeneratorWallJump : LevelGenerator {
     bool wallBuilt;
     CCNode* wallJumpEnd;
     NSMutableArray *walls;
+    NSMutableArray *spawners;
+    NSMutableArray *obstacles;
 }
 
 - (void)initializeLevel:(NSArray*)g :(NSArray*)gc :(Player*)p :(CCPhysicsNode*)pn :(CCNode*) _wn;
+- (void)insertSpawner :(float)posx :(float)dimx;
 
 
 @end
